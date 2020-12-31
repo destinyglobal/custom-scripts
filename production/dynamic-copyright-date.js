6dff9ecd68dd838af01251373eff4b29f837b96a
jeremyholden <41959702+jeremyholden@users.noreply.github.com>
2020-12-31 11:22:03 -0600

    name function correctly

--- production/dynamic-copyright-date.js
+++ production/dynamic-copyright-date.js
@@ -1,4 +1,4 @@
-function(addCopyrightDate) {
+function addCopyrightDate {
 	$('[data-title*="copyright"]').each(function() {
 		var source = $(this).html();
 		var thisYear = new Date().getFullYear();
