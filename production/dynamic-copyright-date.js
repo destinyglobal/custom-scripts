0f90215a5427fe9566d9e44caa9737bfa1c32bc3
Jeremy <jholden@danijohnson.com>
2021-01-29 09:49:30 -0600

    set correct handlebars template to look for

--- production/dynamic-copyright-date.js
+++ production/dynamic-copyright-date.js
@@ -2,7 +2,7 @@ function addCopyrightDate() {
 	$('[data-title*="copyright"],[data-title*="copywrite"]').each(function() {
 		var source = $(this).html();
 		var thisYear = new Date().getFullYear();
-		var yearDisplay = { arr:["copyrightdate","copywrite"]:thisYear.toString()}
+		var yearDisplay = { arr:["copyrightdate","copywritedate"]:thisYear.toString()}
 		var template = Handlebars.compile(source);
 		$(this).html(template(yearDisplay));
 	});
